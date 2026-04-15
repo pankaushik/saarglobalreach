46055eb5e01186514dedfbcbb36c094d7425ac4d
Pankaj Kaushik <pankaushik@gmail.com>
2026-04-15 22:45:13 +0530

    Second commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : decodeURIComponent(req.url));
+
+  const ext = path.extname(filePath);
+  let contentType = 'text/html';
+  switch (ext) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.avif':
+      contentType = 'image/avif';
+      break;
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(8000, () => {
+  console.log('Server running at http://localhost:8000');
+});
